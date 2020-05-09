6c4a0e51961fe817b863832f550ebbb8799b00a3
哈哈哈哈 <64090718+hello0ss@users.noreply.github.com>
2020-05-09 17:10:24 +0800

    Update AliyunOssUtil.java

--- src/main/java/com/ss/util/AliyunOssUtil.java
+++ src/main/java/com/ss/util/AliyunOssUtil.java
@@ -14,8 +14,8 @@ public class AliyunOssUtil {
     //上传文件至阿里云
     private static String endpoint = "http://oss-cn-beijing.aliyuncs.com";
     // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-    private static String accessKeyId = "LTAI4FpUFv22VFB6HeRnnCSp";
-    private static String accessKeySecret = "W81MUw08Q52f6qXQ53XYvTigeJJBpT";
+    private static String accessKeyId = "";
+    private static String accessKeySecret = "";
 
     // 创建OSSClient实例。
     private static OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
